74f54a80b5707b147d70747c555a0aa519959a6f
Elijah C <ejd.christensen@gmail.com>
2018-09-04 23:08:48 -0600

    new zsh things

--- init.sh
+++ init.sh
@@ -8,5 +8,10 @@ ln -sf $HOME/.dotfiles/vim/plug.vim $HOME/.vimrc
 ln -sf $HOME/.dotfiles/tmux/ $HOME/.tmux
 ln -sf $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
+# zsh
+ln -sf $HOME/.dotfiles/zsh/ $HOME/.tmux
+ln -sf $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc
+
+
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
