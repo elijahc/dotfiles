1e3c62ab329c981485d4c442434d5a3b6080a2c8
Elijah C <ejd.christensen@gmail.com>
2018-09-04 23:40:44 -0600

    fixed bug creating self-referential symlinks

--- init.sh
+++ init.sh
@@ -1,17 +1,20 @@
-cd ~
+cd $HOME
 
 # Vim
-ln -sf $HOME/.dotfiles/vim ~/.vim
-ln -sf $HOME/.dotfiles/vim/plug.vim $HOME/.vimrc
+ln -sfT $HOME/.dotfiles/vim/plug.vim $HOME/.vimrc
+ln -sfT $HOME/.dotfiles/vim/ $HOME/.vim
 
 #Tmux
-ln -sf $HOME/.dotfiles/tmux/ $HOME/.tmux
-ln -sf $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -sfT $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -sfT $HOME/.dotfiles/tmux/ $HOME/.tmux
 
 # zsh
-ln -sf $HOME/.dotfiles/zsh/ $HOME/.tmux
-ln -sf $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc
+ln -sfT $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc
+ln -sfT $HOME/.dotfiles/zsh/ $HOME/.zsh
 
 
-mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+if [ ! -d $HOME/.tmux/plugins ]; then
+    echo "~/.tmux/plugins doesn't exist; creating..."
+    mkdir -p ~/.tmux/plugins
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
